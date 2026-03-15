c693b74b193170ded61b606afba15eb26e184dab
panboren <384953161@qq.com>
2026-03-15 23:42:44 +0800

    init

---
+++ src/views/web-demo/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="web-demo">
+    宠物官网网页
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.web-demo {
+  min-height: 100vh;
+  background: linear-gradient(135deg, #0a0a1a 0%, #1a1a3a 50%, #0a0a2a 100%);
+  overflow-x: hidden;
+  position: relative;
+}
+</style>
